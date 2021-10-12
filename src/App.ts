bd4f02f120aa08d1fdf5242d244998e615ada403
Aleksandr Kanunnikov <lifeart92@gmail.com>
2021-10-12 03:27:10 +0300

    logo example

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,6 @@
 import Component, { hbs, tracked } from '@glimmerx/component';
 import { on, action } from '@glimmerx/modifier';
+import logo from './assets/glimmer-logo.png';
 
 import HelloWorld from './components/HelloWorld.hbs';
 
@@ -8,7 +9,9 @@ const Heading = hbs`<h1>Hello {{@bundlerName}}!</h1>`;
 const DocumentationLink = hbs`<a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>`;
 export default class App extends Component {
     @tracked bundlerName = 'vite';
+    assets = { logo };
     static template = hbs`
+        <img width=50 height=50 src={{this.assets.logo}} />
         <input {{on 'input' this.updateValue}} value={{this.bundlerName}} />
         <HelloWorld />
         <Heading @bundlerName={{this.bundlerName}} />
