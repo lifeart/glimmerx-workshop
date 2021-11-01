8994d0a2bf075e22276f4b76f13a5cdd78a80adb
Aleksandr Kanunnikov <lifeart92@gmail.com>
2021-11-01 13:40:25 +0300

    +

--- src/entry-server.ts
+++ src/entry-server.ts
@@ -1,16 +1,18 @@
 import 'glimmer-apollo/dist/cjs/environment-glimmer';
 import { renderToString } from '@glimmerx/ssr';
-import App from './App';
+import { hbs } from '@glimmerx/component';
+// import App from './App';
 // import setupApolloClient from './configs/apollo';
 import { router } from './router';
 import { createSearchParams } from './utils/search-params';
 // setupApolloClient(undefined);
 
+const app = hbs``;
 
 export async function render(url: string) {
     createSearchParams(url.split('?')[1] || '');
     await router.mount(url, true);
-    return renderToString(App, {
+    return renderToString(app, {
         rehydrate: true,
         services: {
             router
