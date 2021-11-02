f670e1ba873440f96bd875b974cc550868eb0674
Aleksandr Kanunnikov <lifeart92@gmail.com>
2021-11-02 14:34:51 +0300

    fix naming

--- types/global.d.ts
+++ types/global.d.ts
@@ -2,8 +2,8 @@
 declare module '*.hbs' {
     // function template(arguments?: Record<string, unknown>) : string | null;
     function templateWrapper() : string | null;
-    const template: string | null;
+    const glimmerTpl: string | null;
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     // type Template = any;
-    export default template;
+    export default glimmerTpl;
 }
